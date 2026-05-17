ad3d268b12f98c131f900e0c254c0413809cc959
xxSeasonxx <season.syys@outlook.com>
2026-05-17 10:18:37 -0400

    Cover simple momentum rule boundaries

--- tests/test_simple_momentum.py
+++ tests/test_simple_momentum.py
@@ -48,6 +48,24 @@ def test_generate_signals_uses_current_bar_as_decision_time_without_lookahead():
     ]
 
 
+def test_generate_signals_stops_after_first_positive_close_change():
+    signals = generate_signals(bars_for([100.0, 101.0, 102.0, 103.0]), {"weight": 1.0, "hold_bars": 1})
+
+    assert signals == [
+        {
+            "symbol": "DEMO",
+            "decision_time": datetime(2024, 1, 2, tzinfo=timezone.utc),
+            "side": "long",
+            "weight": 1.0,
+            "hold_bars": 1,
+        }
+    ]
+
+
 def test_generate_signals_returns_empty_list_for_degenerate_inputs():
     assert generate_signals([], {}) == []
     assert generate_signals(bars_for([100.0]), {}) == []
+
+
+def test_generate_signals_returns_empty_list_without_positive_close_change():
+    assert generate_signals(bars_for([100.0, 100.0, 99.0]), {}) == []
